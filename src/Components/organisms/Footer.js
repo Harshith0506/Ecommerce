4ec4aa4247f5b9d572b60e2e9426d98520587579
Harshith Kumar L <harshithkumar.l@zensar.com>
2024-02-14 23:08:28 +0530

    third commit

--- src/Components/organisms/Footer.js
+++ src/Components/organisms/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="border-y-2 border-gray-200 h-[400px]">
-      <div className="h-[460px] flex">
+    <div className="border-t-2 border-gray-200 ">
+      <div className="flex">
         <div>
           <div className="object-contain cursor-pointer mx-16 my-8 border-b-2 w-[250px] px-6">
             <img
@@ -12,41 +12,65 @@ const Footer = () => {
               className="h-36 w-48"
             />
           </div>
-          <p className="w-[280px] px-4 mx-12 font-medium text-sm text-justify text-gray-500">
+          <p className="w-[290px] px-4 mx-12 font-medium text-sm text-justify text-gray-500">
             ShopCart brings you the joy of discovery with a huge selection of
             original products from mobiles, fashion,home appliances, groceries,
             ...
           </p>
         </div>
 
-        <div className=" mx-4 w-[260px] my-9">
-          <h2 className="font-bold text-lg text-[#050533]">Department</h2>
+        <div className=" mx-16 w-[160px] my-9">
+          <h2 className="font-bold text-base text-[#050533]">Department</h2>
           <div>
-            <ul className="text-base font-medium space-y-5 cursor-pointer -mx-2 font-poppins my-5">
-              {/* <li className='flex items-center'><ChevronRightIcon className='footer' />Grievance Addressal</li> */}
-              {/* <li className='flex items-center'><ChevronRightIcon className='footer' />Events and other calendars</li> */}
+            <ul className="text-xs font-medium space-y-5 cursor-pointer -mx-2 font-poppins my-5">
+              <li className="flex items-center">Fashion</li>
+              <li className="flex items-center">Education product</li>
+              <li className="flex items-center">Frozen food</li>
+              <li className="flex items-center">Beverages</li>
+              <li className="flex items-center">Organic Grocery</li>
+              <li className="flex items-center">Office Supplies</li>
+              <li className="flex items-center">Beauty Products</li>
+              <li className="flex items-center">Books</li>
             </ul>
           </div>
         </div>
 
-        <div className=" mx-3 my-9 w-[260px]">
-          <h2 className="font-bold text-lg text-[#050533]">About US</h2>
+        <div className=" mx-5 my-9 w-[170px]">
+          <h2 className="font-bold text-base text-[#050533]">About US</h2>
           <div>
-            <ul className="text-base font-medium space-y-5 cursor-pointer -mx-2 font-poppins my-5">
-              {/* <li className='flex items-center'><ChevronRightIcon className='footer' />Online Fees Payment</li> */}
-              {/* <li className='flex items-center'><ChevronRightIcon className='footer' />Sports</li> */}
+            <ul className="text-xs font-medium space-y-5 cursor-pointer -mx-2 font-poppins my-5">
+              <li className="flex items-center">About shopcart</li>
+              <li className="flex items-center">Careers</li>
+              <li className="flex items-center">News & Blog</li>
+              <li className="flex items-center">Help</li>
+              <li className="flex items-center">Press Center</li>
+              <li className="flex items-center">Shop by location</li>
+              <li className="flex items-center">Ideas & guides</li>
             </ul>
           </div>
         </div>
 
-        <div className=" mx-3 my-9 w-[270px]">
-          <h2 className="font-bold text-lg text-[#050533]">Services</h2>
+        <div className=" mx-5 my-9 w-[180px]">
+          <h2 className="font-bold text-base text-[#050533]">Services</h2>
           <div>
-            <ul className="text-base font-medium space-y-5 cursor-pointer -mx-2 font-poppins my-5">
-              <li className="flex items-center">RPG Group of Engineering</li>
-              <li className="flex items-center">RPG Group of Engineering</li>
-              <li className="flex items-center">RPG Group of Engineering</li>
-              <li className="flex items-center">RPG Group of Engineering</li>
+            <ul className="text-xs font-medium space-y-5 cursor-pointer -mx-2 font-poppins my-5">
+              <li className="flex items-center">Gift Card</li>
+              <li className="flex items-center">Mobile App</li>
+              <li className="flex items-center">Shipping and Delivery</li>
+              <li className="flex items-center">Order Pickup</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className=" mx-3 my-9 w-[160px]">
+          <h2 className="font-bold text-base text-[#050533]">Help</h2>
+          <div>
+            <ul className="text-xs font-medium space-y-5 cursor-pointer -mx-2 font-poppins my-5">
+              <li className="flex items-center">ShopCart help</li>
+              <li className="flex items-center">Returns</li>
+              <li className="flex items-center">Track orders</li>
+              <li className="flex items-center">Contact us</li>
+              <li className="flex items-center">Feedback</li>
             </ul>
           </div>
         </div>
